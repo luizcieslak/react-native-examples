b0ef6ea4e9ce83366cbdd471c4f9dd391d2974a0
Luiz Fernando da Silva Cieslak <cieslakluiz@gmail.com>
2019-01-15 21:45:34 -0200

    react-nav: fixing bug related to gestures in Android

--- react-navigation/navigation/DrawerNavigator.js
+++ react-navigation/navigation/DrawerNavigator.js
@@ -5,9 +5,11 @@ import { createDrawerNavigator } from 'react-navigation'
 import Drawer1 from '../components/Drawer1'
 import Drawer2 from '../components/Drawer2'
 
+// Don't forget to change MainActivity.java after linking react-native-gesture-handler
+// https://stackoverflow.com/questions/53394982/react-navigation-swipe-on-drawer-does-not-work-in-android
 export default createDrawerNavigator({
-  Drawer1: {
-    screen: Drawer1
+    Drawer1: Drawer1,
+    Drawer2: Drawer2
   },
   Drawer2: {
     screen: Drawer2
