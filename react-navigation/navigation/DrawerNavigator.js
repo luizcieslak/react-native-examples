6710df6fbcc87e47709f29ef2de767bd8557d522
Luiz Fernando da Silva Cieslak <cieslakluiz@gmail.com>
2019-01-15 21:52:42 -0200

    react-nav: custom icons in drawer items

--- react-navigation/navigation/DrawerNavigator.js
+++ react-navigation/navigation/DrawerNavigator.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import Image from 'react-native'
 import { createDrawerNavigator } from 'react-navigation'
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 import Drawer1 from '../components/Drawer1'
 import Drawer2 from '../components/Drawer2'
@@ -10,8 +10,23 @@ import Drawer2 from '../components/Drawer2'
 export default createDrawerNavigator({
     Drawer1: Drawer1,
     Drawer2: Drawer2
-  },
-  Drawer2: {
-    screen: Drawer2
-  }
+}, {
+  drawerPosition: 'left',
+  //centralizing tabBarIcon config, same in TabNavigator
+  defaultNavigationOptions: ({ navigation }) => ({
+    headerTintColor: '#000',
+    headerStyle: {
+      backgroundColor: '#fff',
+    },
+    drawerIcon: ({ focused, horizontal, tintColor }) => {
+      let iconName
+      if (navigation.state.routeName === 'Drawer1') {
+        iconName = `ios-add-circle${focused ? '' : '-outline'}`
+      } else if (navigation.state.routeName === 'Drawer2') {
+        iconName = `ios-battery-${focused ? 'charging' : 'dead'}`
+      }
+
+      return <Ionicons name={iconName} size={horizontal ? 20 : 25} color={tintColor} />
+    }
+  })
 })
